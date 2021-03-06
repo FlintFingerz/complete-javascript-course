0e00731a4483ec9d21588f88afbc073ca3883934
FlintFingerz <36845591+FlintFingerz@users.noreply.github.com>
2021-03-06 16:20:20 -0700

    updates

---
+++ 09-Data-Structures-Operators/starter/challenge4.js
@@ -0,0 +1,47 @@
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+// let underscoreCase = [];
+
+// 1. Split all of the test data into an array, separated by the \n character
+const button = document.querySelector(`button`);
+const camelCase = function () {
+  const text = document.querySelector('textarea').value;
+  const underscoreSplit = text.split(`\n`);
+  console.log(underscoreSplit);
+  const wordsSplit = [];
+  const wordsSecond = [];
+  const wordsUpper = [];
+  let i = 0;
+
+  // 2. Loop over the array we created and split each element by the underscore (_)
+  //   for (const n of underscoreSplit) {
+  //     i++;
+  //     const [first, second] = n.toLowerCase().trim().split(`_`);
+  //     const output = `${first}${second.replace(
+  //       second[0],
+  //       second[0].toUpperCase()
+  //     )}`;
+  //     console.log(`${output.padEnd(20, ` `)}${`✅`.repeat(i)}`);
+  //   }
+  // };
+
+  // How Jonas did the above, instead of declaring i and then increasing i every iteration, he destructured and looped over underscoreSplit.entries
+  for (const [i, n] of underscoreSplit.entries()) {
+    const [first, second] = n.toLowerCase().trim().split(`_`);
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20, ` `)}${`✅`.repeat(i + 1)}`);
+  }
+};
+
+button.addEventListener(`click`, camelCase);
+
+/*
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+*/
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -51,7 +51,172 @@ const restaurant = {
   },
 };
 
+/////////////////////////////////
+// Working With Strings - Part 3
+console.log(`a+very+nice+string`.split(`+`));
+console.log(`Jonas Scmedtmann`.split(` `));
+
+// Split method splits a string into multiple elements of an array. It takes an argument the is the deciding point on where to split.
+const [firstName, lastName] = `Jonas Scmedtmann`.split(` `);
+
+// Join method joins multiple elements in an array into a string. It takes an argument which is inserted in between the element values after joining. If you don't provide any argument, it will separate them by commas. If you provide an empty string, it will join them all together with no spaces.
+const newName = [`Mr.`, firstName, lastName.toUpperCase()].join(` `);
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(` `);
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(` `));
+};
+
+capitalizeName(`jessica ann smith davis`);
+capitalizeName(`jonas schmedtmann`);
+
+// Padding
+const message = `Go to gate 23!`;
+console.log(message.padStart(20, `+`).padEnd(30, `+`));
+console.log(`Jonas`.padStart(20, `+`).padEnd(30, `+`));
+
+// This takes number, converts it to a tring, creates a new variable composed of the last 4 positions of the string we just converted number to, then returns that new string (last) as the same length as the original string (str) using * to fill in the extra space at the beginning.
+const maskCreditCard = function (number) {
+  const str = number + ``;
+  const last = str.slice(-4);
+  return last.padStart(str.length, `*`);
+};
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(4337846386468875));
+console.log(maskCreditCard(`334565484616516843513`));
+
+// Repeat
+const message2 = `Bad weather... All Departures Delayed... `;
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${`✈`.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
 /*
+/////////////////////////////
+// Working With Strings - Part 2
+const airline = `Tap Air Portugal`;
+
+console.log(airline.toLowerCase());
+console.log(`jonas`.toUpperCase());
+
+// Fix capitalization in name
+const passenger = `jOnAS`; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// Check email
+const email = `hello@jonas.io`;
+const loginEmail = `  Hello@Jonas.Io \n`;
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// Created a function to check a provided email against a correct email and return true if they are the same after converting to lower case and trimming.
+// const fixedEmail = function (correctEmail, givenEmail) {
+//   adjustedEmail = givenEmail.toLowerCase().trim();
+//   return adjustedEmail === correctEmail;
+// };
+
+// console.log(fixedEmail(`soren@avery.com`, `  Soren@AVery.cOm`));
+
+// Replacing
+const priceGB = `288,97£`;
+const priceUS = priceGB.replace(`£`, `$`).replace(`,`, `.`);
+console.log(priceUS);
+
+const announcement = `All passengers come to boarding door 23. Boarding door 23!`;
+
+console.log(announcement.replace(`door`, `gate`));
+// console.log(announcement.replaceAll(`door`, `gate`)); // Works now but not when video was made
+console.log(announcement.replace(/door/g, `gate`));
+
+// Booleans
+const plane = `Airbus A320neo`;
+console.log(plane.includes(`A320`));
+console.log(plane.includes(`Boeing`));
+console.log(plane.startsWith(`Air`));
+
+if (plane.startsWith(`Airbus`) && plane.endsWith(`neo`)) {
+  console.log(`Part of the NEW Airbus family`);
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes(`knife`) || baggage.includes(`gun`)) {
+    console.log(`You are NOT allowed on board`);
+  } else {
+    console.log(`Welcome aboard!`);
+  }
+};
+checkBaggage(`I have a laptop, some Food and a pocket Knife`);
+checkBaggage(`Socks and camera`);
+checkBaggage(`Got some snacks and a gun for protection`);
+
+
+/////////////////////////////
+// Working With Strings Part 1
+// Use slice -1 to start from the right
+const airline = `TAP Air Portugal`;
+const plane = `A320`;
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log(`B737`[2]);
+
+console.log(airline.length);
+console.log(`B737`.length);
+
+console.log(airline.indexOf(`r`));
+console.log(airline.lastIndexOf(`r`));
+console.log(airline.lastIndexOf(`portugal`));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(` `)));
+console.log(airline.slice(airline.lastIndexOf(` `) + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === `B` || s === `E`) console.log(`You got the middle seat 😬`);
+  else console.log(`You got lucky 😎`);
+};
+
+checkMiddleSeat(`11B`);
+checkMiddleSeat(`23C`);
+checkMiddleSeat(`3E`);
+
+console.log(new String(`jonas`));
+console.log(typeof new String(`jonas`));
+
+console.log(typeof new String(`jonas`).slice(1));
+
+
 ///////////////////////////////
 // Summary: Which Data Structure To Use?
 // Arrays or Sets: Simple list of values.
